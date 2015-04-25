5edfc7329ddf85e6562dbc7bd4ba3ebe949f428c
Yavor Paunov <hello@yavorpaunov.com>
2015-04-26 00:06:24 +0200

    Reorganized localization

--- time2words/__init__.py
+++ time2words/__init__.py
@@ -4,20 +4,21 @@ from utils import normalize
 from chain import Chain
 from commands import LessThan1M, LessThan1H, LessThan23H, LessThan6D1H
 from commands import LessThan25D10H, LessThan11MM, LessThan10Y, MoreThan10Y
+from localization import locales, _default
 
 __version__ = '0.0.2'
 
 
-def approx_time(loc, **kwargs):
+def approx_time(l10n=locales.get(_default), **kwargs):
     kwargs = normalize(**kwargs)
 
     cor = Chain()
-    cor.add(LessThan1M(loc, **kwargs))
-    cor.add(LessThan1H(loc, **kwargs))
-    cor.add(LessThan23H(loc, **kwargs))
-    cor.add(LessThan6D1H(loc, **kwargs))
-    cor.add(LessThan25D10H(loc, **kwargs))
-    cor.add(LessThan11MM(loc, **kwargs))
-    cor.add(LessThan10Y(loc, **kwargs))
-    cor.add(MoreThan10Y(loc, **kwargs))
+    cor.add(LessThan1M(l10n, **kwargs))
+    cor.add(LessThan1H(l10n, **kwargs))
+    cor.add(LessThan23H(l10n, **kwargs))
+    cor.add(LessThan6D1H(l10n, **kwargs))
+    cor.add(LessThan25D10H(l10n, **kwargs))
+    cor.add(LessThan11MM(l10n, **kwargs))
+    cor.add(LessThan10Y(l10n, **kwargs))
+    cor.add(MoreThan10Y(l10n, **kwargs))
     return cor.run()
